bb596d37145ad4189cc63ca34853163671ab72e8
Mark Ulrich <markulrich@users.noreply.github.com>
2018-03-17 14:47:33 -0700

    Use pagination and async functions for backup

--- backup.js
+++ backup.js
@@ -1,4 +1,4 @@
-import * as firebase from 'firebase';
+import firebase from 'firebase';
 import 'firebase/firestore';
 import fs from 'fs';
 
@@ -9,27 +9,31 @@ const db = firebase.firestore();
 const queries = {
     'operations': db.collection('operations'),
     'members': db.collection('members'),
-    'relations': db.collection('relations'),
-    'user_data': db.collection('user_data')
 };
 
-let queriesCompleted = 0;
-
-Object.entries(queries).forEach((nmQry) => {
-    let [name, query] = nmQry;
-    query.get().then(function(snap) {
+async function storeQueryResult(nameAndQuery) {
+    let [name, query] = nameAndQuery;
+    let backup = [];
+    while (true) {
+        let snap = await query.get();
+        let fetchSize = snap.docs.length;
+        if (fetchSize === 0) break;
+        console.log(`Fetched ${fetchSize} from ${name}`);
         let data = snap.docs.map(x => JSON.stringify({
             id: x.id,
             data: x.data(),
         }));
-        fs.writeFile('./backup.' + name + '.json', data.join('\n'), (err) => {
-            if (err) console.error(err);
-            else console.log(data.length + ' records written for ' + name + '.')
+        backup = backup.concat(data);
+        let last = snap.docs[snap.docs.length - 1];
+        query = query.startAfter(last);
+    }
+    await fs.writeFile('./backup.' + name + '.json', backup.join('\n'));
+    console.log(`${backup.length} records written for ${name}.`);
+}
 
-            // Teminate after having completed all queries.
-            if (++queriesCompleted >= Object.keys(queries).length) {
-                process.exit();
-            }
-        });
-    });
-});
+async function storeAllQueries() {
+    await Promise.all(Object.entries(queries).map(storeQueryResult));
+    process.exit();
+}
+
+storeAllQueries();
