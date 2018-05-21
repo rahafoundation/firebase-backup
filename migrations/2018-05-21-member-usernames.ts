d2560eab2c111a17de8ed68cd0836d3245906493
Omar Diab <me@omardiab.com>
2018-05-21 13:30:02 -0700

    Remove old migrate-member-usernames, add timestamp to migration filename

--- migrations/2018-05-21-member-usernames.ts
+++ migrations/2018-05-21-member-usernames.ts
@@ -4,17 +4,29 @@
  */
 import * as admin from "firebase-admin";
 
-async function migrateMemberUsernamesDollarToDot(pathToFbKey: string, projectId: string): Promise<void> {
+async function migrateMemberUsernamesDollarToDot(
+  pathToFbKey: string,
+  projectId: string
+): Promise<void> {
   const firebaseKey = require(pathToFbKey);
   if (firebaseKey.project_id !== projectId) {
-    throw Error(`Gave projectId ${projectId} but path to firebase key credentials was for ${firebaseKey.project_id}`);
+    throw Error(
+      `Gave projectId ${projectId} but path to firebase key credentials was for ${
+        firebaseKey.project_id
+      }`
+    );
   }
   const app = admin.initializeApp({
     credential: admin.credential.cert(firebaseKey),
     databaseURL: `https://${firebaseKey.project_id}.firebaseio.com`
   });
   const db = app.firestore();
-  const GENESIS_UIDS = new Set(['mPTv77Bs8iVdPXUzF7bYv2zZ0qI2', 'sj7oal4vnLUALUhE5tb6xazgmuI3', 'dgSRh23dMcOXQeoA0vUyBE4cVAD2', 'Cj4TcLjooRe2D4WfIZyPAzdlUT52']);
+  const GENESIS_UIDS = new Set([
+    "mPTv77Bs8iVdPXUzF7bYv2zZ0qI2",
+    "sj7oal4vnLUALUhE5tb6xazgmuI3",
+    "dgSRh23dMcOXQeoA0vUyBE4cVAD2",
+    "Cj4TcLjooRe2D4WfIZyPAzdlUT52"
+  ]);
   const numDocs = await db.runTransaction(async tx => {
     const membersRef = db.collection("members");
     const opsRef = db.collection("operations");
@@ -22,7 +34,7 @@ async function migrateMemberUsernamesDollarToDot(pathToFbKey: string, projectId:
     const operations = await opsRef.get();
     const memberUidToMid = new Map();
     members.docs.forEach(d => {
-      let mid = d.get('mid');
+      let mid = d.get("mid");
       if (!mid) {
         throw Error(`Found an mid with value ${mid} for member doc ${mid}`);
       }
@@ -31,35 +43,54 @@ async function migrateMemberUsernamesDollarToDot(pathToFbKey: string, projectId:
       tx.update(membersRef.doc(d.id), {
         mid: admin.firestore.FieldValue.delete(),
         request_invite_from_mid: admin.firestore.FieldValue.delete(),
-        username: mid.replace('$', '.')
-      })
+        username: mid.replace("$", ".")
+      });
     });
     operations.docs.forEach(d => {
-      const creatorMid = d.get('creator_mid');
-      const creatorUid = d.get('creator_uid');
-      const toMid = d.get('data.to_mid');
-      const toUid = d.get('data.to_uid');
+      const creatorMid = d.get("creator_mid");
+      const creatorUid = d.get("creator_uid");
+      const toMid = d.get("data.to_mid");
+      const toUid = d.get("data.to_uid");
       if (!creatorMid || !toMid) {
-        if (creatorUid === null && GENESIS_UIDS.has(toUid) && d.get('op_code') === 'TRUST') {
+        if (
+          creatorUid === null &&
+          GENESIS_UIDS.has(toUid) &&
+          d.get("op_code") === "TRUST"
+        ) {
           // okay
-        } else if (GENESIS_UIDS.has(creatorUid) && toUid === null && d.get('op_code') === 'REQUEST_INVITE') {
+        } else if (
+          GENESIS_UIDS.has(creatorUid) &&
+          toUid === null &&
+          d.get("op_code") === "REQUEST_INVITE"
+        ) {
           // okay
         } else {
-          console.error(`Unexpected document: ${JSON.stringify(d.data(), null, 2)}`);
+          console.error(
+            `Unexpected document: ${JSON.stringify(d.data(), null, 2)}`
+          );
         }
-      } else if (memberUidToMid.get(creatorUid) !== creatorMid || memberUidToMid.get(toUid) !== toMid) {
-        console.error(`The op ${d.id} has inconsistent mid and uid mappings, expect ${memberUidToMid.get(creatorUid)} == ${creatorMid} and ${memberUidToMid.get(toUid)} == ${toMid}`);
+      } else if (
+        memberUidToMid.get(creatorUid) !== creatorMid ||
+        memberUidToMid.get(toUid) !== toMid
+      ) {
+        console.error(
+          `The op ${
+            d.id
+          } has inconsistent mid and uid mappings, expect ${memberUidToMid.get(
+            creatorUid
+          )} == ${creatorMid} and ${memberUidToMid.get(toUid)} == ${toMid}`
+        );
       }
       const updates: any = {
         creator_mid: admin.firestore.FieldValue.delete(),
-        'data.to_mid': admin.firestore.FieldValue.delete()
+        "data.to_mid": admin.firestore.FieldValue.delete()
       };
-      if (d.get('op_code') === 'REQUEST_INVITE') {
-        updates['data.username'] = creatorMid.replace('$', '.')
+      if (d.get("op_code") === "REQUEST_INVITE") {
+        updates["data.username"] = creatorMid.replace("$", ".");
       }
-      tx.update(opsRef.doc(d.id), updates)
+      tx.update(opsRef.doc(d.id), updates);
     });
-    return  members.docs.length + operations.docs.length;
+    return members.docs.length + operations.docs.length;
   });
   console.log(`Successfully updates ${numDocs} docs`);
   process.exit();
@@ -67,10 +98,10 @@ async function migrateMemberUsernamesDollarToDot(pathToFbKey: string, projectId:
 
 async function main() {
   const args = process.argv;
-  
+
   if (args.length !== 4) {
     console.error(
-      'Usage is npx ts-node member-usernames.ts path-to-fb-key fb-url. You provided:',
+      "Usage is npx ts-node 2018-05-21-member-usernames.ts path-to-fb-key fb-url. You provided:",
       args
     );
     process.exit(2);
