904394fd79855c272c9c9c1363b4ea3abfbcd7a4
Mark Ulrich <markulrich@users.noreply.github.com>
2018-09-16 16:25:44 -0700

    Add email from auth to firestore

--- migrations/helpers.ts
+++ migrations/helpers.ts
@@ -1,11 +1,16 @@
 import * as path from "path";
 
 import * as admin from "firebase-admin";
+import {
+  Query,
+  QueryDocumentSnapshot,
+  CollectionReference
+} from "@google-cloud/firestore";
 
 export function getDb(pathToFbKey: string, projectId: string) {
-  const firebaseKey = require(path.resolve(
-    path.join(process.cwd(), pathToFbKey)
-  ));
+  const firebaseKey = require(pathToFbKey[0] == "/"
+    ? pathToFbKey
+    : path.resolve(path.join(process.cwd(), pathToFbKey)));
   if (firebaseKey.project_id !== projectId) {
     throw Error(
       `Gave projectId ${projectId} but path to firebase key credentials was for ${
@@ -18,4 +23,22 @@ export function getDb(pathToFbKey: string, projectId: string) {
     databaseURL: `https://${firebaseKey.project_id}.firebaseio.com`
   });
   return app.firestore();
+}
+
+export async function fetchEntireCollection(
+  collection: CollectionReference
+): Promise<QueryDocumentSnapshot[]> {
+  let backup: any[] = [];
+  let curCollectionView: Query = collection;
+  let docs: QueryDocumentSnapshot[] = [];
+  while (true) {
+    const snapshot = await curCollectionView.get();
+    const fetchSize = snapshot.docs.length;
+    if (fetchSize === 0) break;
+    console.log(`Fetched ${fetchSize}`);
+    docs = docs.concat(snapshot.docs as QueryDocumentSnapshot[]);
+    const last = snapshot.docs[snapshot.docs.length - 1];
+    curCollectionView = collection.startAfter(last);
+  }
+  return docs;
 }
