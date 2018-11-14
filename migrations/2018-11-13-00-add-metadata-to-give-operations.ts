a9b61a78e3228431a5bb412938774f0211b196b8
Tina Roh <tinaroh@users.noreply.github.com>
2018-11-14 15:30:13 +0900

    Add Give migration to have metadata and give type (#28)
    * Add Give migration to have metadata and give type
    
    * Fix comment

---
+++ migrations/2018-11-13-00-add-metadata-to-give-operations.ts
@@ -0,0 +1,53 @@
+/**
+ * Add the metadata block including GiveType.DIRECT_GIVE to all legacy GIVE operations. In preparation for tipping functionality.
+ */
+
+import * as path from "path";
+
+import * as admin from "firebase-admin";
+import {
+  Firestore,
+  QueryDocumentSnapshot,
+  DocumentSnapshot
+} from "@google-cloud/firestore";
+
+import { getDb } from "../helpers";
+import { request } from "http";
+
+async function addMetadataToGiveOperations(db: Firestore) {
+  const giveOperations = (await db
+    .collection("operations")
+    .where("op_code", "==", "GIVE")
+    .get()).docs;
+  await Promise.all(
+    giveOperations.map(async op => {
+      console.log(op.id);
+      const opData = op.get("data");
+      if ("metadata" in opData) {
+        // Skip if it already has metadata.
+        return;
+      }
+
+      const updatedOpData = {
+        ...opData,
+        metadata: {
+          type: "DIRECT_GIVE",
+          memo: opData.memo
+        }
+      };
+      await op.ref.update({
+        data: updatedOpData
+      });
+    })
+  );
+}
+
+async function main() {
+  const args = process.argv;
+  const db = getDb(args[2], args[3]);
+  await addMetadataToGiveOperations(db);
+}
+
+main().then(() => {
+  process.exit(0);
+});
